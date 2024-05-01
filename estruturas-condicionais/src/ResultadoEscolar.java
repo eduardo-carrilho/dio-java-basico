bb683faa0a92118bae96477fedcd1dc3bba49e6a
eduardo-carrilho <dudu_carrilho@hotmail.com>
2024-05-01 18:05:06 -0300

    final da segunda aula sobre estruturas condicionais

--- estruturas-condicionais/src/ResultadoEscolar.java
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -1,10 +1,15 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 19;
 
         if(nota >=7){
             System.out.println("Aprovado");
-        }else{
+        
+        }else if (nota >= 5 && nota < 7) {
+            System.out.println("Prova Recuperação");
+        }
+        
+        else{
             System.out.println("Reprovado");
         }
     }
