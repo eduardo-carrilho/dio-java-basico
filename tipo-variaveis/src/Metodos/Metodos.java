14284be270ae75c262900f36e4793adf55d7b50b
eduardo-carrilho <dudu_carrilho@hotmail.com>
2024-05-01 15:11:12 -0300

    fim das aulas sobre métodos

---
+++ tipo-variaveis/src/Metodos/Metodos.java
@@ -0,0 +1,11 @@
+package Metodos;
+public class Metodos {
+
+    
+    public static void main(String[] args) {
+        //Métodos devem ser nomeados como verbo e seguir padrão camelCase
+
+    }
+    
+
+}
