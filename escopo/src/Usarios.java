d5f296150aa5a62259e7459542ddadd0f8ca58e9
eduardo-carrilho <dudu_carrilho@hotmail.com>
2024-05-01 15:34:47 -0300

    feat: final da aula sobre escopo utilizando como exemplos os exercicios da aula anterior

---
+++ escopo/src/Usarios.java
@@ -0,0 +1,42 @@
+
+public class Usarios {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual: "+ smartTv.volume);
+        
+        System.out.println("Canal Atual: "+ smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal Atual: "+ smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        int canalDesejado = 10;
+        
+        smartTv.mudarCanal(canalDesejado);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        
+        System.out.println("TV Ligada? "+ smartTv.ligada);
+
+        smartTv.ligar();
+        System.out.println("TV Ligada? "+ smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV Ligada? "+ smartTv.ligada);
+
+
+
+    
+    
+    
+    
+    }
+    
+
+}
