0bdd889f03bb61e5afe127245a425183580744b8
eduardo-carrilho <dudu_carrilho@hotmail.com>
2024-05-02 12:22:14 -0300

    final da aula sobre estrutura de repetição "for" usada em Arrays

---
+++ estruturas-de-repeticao/src/ExemploForArrays.java
@@ -0,0 +1,9 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é " + aluno);
+        }
+    }
+}
